997b5814960618e0c4e3d93f38cf8fcc4d313e52
Akshay Singare <158290336+AkshaySingare@users.noreply.github.com>
2025-03-18 23:10:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+/* var randomNumber=Math.floor(Math.random()*1)+1;
+var x = computer(randomNumber)
+function computer(){
+  if(randomNumber==1){
+  $(".anime").animate({
+    Width:"toggle"
+  },1000);
+  };
+}*/
+
+
+
+// home serch----->
+document.getElementById('search-button').addEventListener('click', function() {
+  const searchInput = document.getElementById('search-input').value;
+  alert('You searched for: ' + searchInput);
+});
+
+// // About page~
+// // JavaScript code for the About page can include interactivity if needed. 
+// // For now, we'll just include a simple example of a function to display an alert message.
+// document.addEventListener('DOMContentLoaded', function() {
+//   // Example: Display a welcome message when the About page loads.
+//   alert('Welcome to the About Us page of Recipe & Food Order website!');
+// });
+
+
+// // Q&A page~
+
+
+
+
+
